bcae4fa97841f4f7920baff81a4d61ae53befbc3
vmadylius <vladyslav.madylius.pp.2023@lpnu.ua>
2026-05-17 23:14:33 +0300

    feat: set base title on main

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
 
   return (
     <div style={containerStyle}>
-      <h1>Trading Sessions Dashboard</h1>
+      <h1>Старий Заголовок</h1>
       <p>Click a button to analyze session characteristics:</p>
       
       <div>
