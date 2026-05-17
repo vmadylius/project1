9be7f8b3c8db5ca71669e52d1d9a143e2bdff46f
vmadylius <vladyslav.madylius.pp.2023@lpnu.ua>
2026-05-18 02:35:25 +0300

    fix: усунено дублювання імпорту App у main.jsx для успішного лінтингу

--- src/main.jsx
+++ src/main.jsx
@@ -2,32 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Імпортуємо аналітику PostHog [cite: 144]
+import posthog from 'posthog-js'
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// Імпортуємо аналітику
-import posthog from 'posthog-js' // [cite: 144]
-
-// Активуємо трекінг перед рендером інтерфейсу
-posthog.init('phc_kZGdUhatELsSuYfZNAWJL8KVSFLKJsNWzkPY7ojitrAT', { // [cite: 145]
-  api_host: 'https://eu.posthog.com',           // [cite: 146]
-  person_profiles: 'identified_only',             // [cite: 147]
+// Активуємо трекінг перед рендером інтерфейсу [cite: 145]
+posthog.init('phc_kZGdUhatELsSuYfZNAWJL8KVSFLKJsNWzkPY7ojitrAT', {
+  api_host: 'https://eu.posthog.com', // [cite: 146]
+  person_profiles: 'identified_only', // [cite: 147]
 })
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
+// Єдиний правильний рендер застосунку
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
